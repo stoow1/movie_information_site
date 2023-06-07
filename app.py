7b1048f8e84d911c6cd3553ceec7d99b08b6972d
gyeong deok <107936957+littleduck1219@users.noreply.github.com>
2023-06-07 21:19:35 +0900

    필요없는 부분 제거

--- app.py
+++ app.py
@@ -9,35 +9,6 @@ app = Flask(__name__)
 client = MongoClient('mongodb+srv://sparta:test@cluster0.qtbduoj.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=certifi.where())
 db = client.dbsparta
 
-# URL = "https://movie.daum.net/ranking/reservation"
-# headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-# data = requests.get(URL, headers=headers)
-# soup = BeautifulSoup(data.text, 'html.parser')
-
-# lis = soup.select("#mainContent > div > div.box_ranking > ol > li")
-# print(lis)
-
-# # 기존 데이터 삭제
-# db.movie.delete_many({})
-
-# for li in lis:
-#     title = li.select_one('.link_txt').text
-#     rate = li.select_one('.txt_num').text
-#     poster = li.select_one('.poster_movie>img')['src']
-#     rank = li.select_one('.rank_num').text
-
-#     if title is not None:
-#         doc = {
-#             'title': title,
-#             'poster': poster,
-#             'rank': rank,
-#             'rate': rate,
-#         }
-#         print(doc)
-#         db.movie.insert_one(doc)
-#     print(rank, title, rate, poster)
-
-
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -50,7 +21,6 @@ def movie_post():
     soup = BeautifulSoup(data.text, 'html.parser')
 
     lis = soup.select("#mainContent > div > div.box_ranking > ol > li")
-    print(lis)
 
     # 기존 데이터 삭제
     db.movie.delete_many({})
@@ -60,6 +30,7 @@ def movie_post():
         rate = li.select_one('.txt_num').text
         poster = li.select_one('.poster_movie>img')['src']
         rank = li.select_one('.rank_num').text
+        url = li.select_one('.link_txt')['href']
 
         if title is not None:
             doc = {
@@ -67,10 +38,10 @@ def movie_post():
                 'poster': poster,
                 'rank': rank,
                 'rate': rate,
+                'url': url
             }
-            print(doc)
             db.movie.insert_one(doc)
-        print(rank, title, rate, poster)
+        print(rank, title, rate, poster, url)
     
     return jsonify({'msg': 'POST 연결 완료!'})
     
